78c764ab05a293e32a4f9f95423ce768d159d1f0
jerrychen <jerrychen040@gmil.com>
2018-04-06 17:07:54 +0800

    update the comment

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -35,7 +35,6 @@ class WaypointUpdater(object):
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
@@ -48,9 +47,12 @@ class WaypointUpdater(object):
         #rospy.spin()
         self.loop()
 
+    #for control the publish frequence
+    # we will publish the wapoints to wapoint_follower
     def loop(self):
-        rate=rospy.Rate(50)# 50hz, change to 30 for later
+        rate=rospy.Rate(50)# 50hz, change to 30 later in driver by wire
         while not rospy.is_shutdown():
+            # if there are pose data in base_waypoits and the rospy not shutdown....
             if self.pose and self.base_waypoints:
                 #Get closest waypoint
                 closest_waypoint_idx = self.get_closest_waypoint_idx()
@@ -59,11 +61,18 @@ class WaypointUpdater(object):
 
 
     def get_closest_waypoint_idx(self):
+        #get thte coordinate of our car
         x=self.pose.pose.position.x
         y=self.pose.pose.position.y
+        # get the index of this x,y in KDtree
         closest_idx = self.waypoint_tree.query([x,y],1)[1]
 
-        #check if closeset is ahed or behind vehicle
+        # we hopt the new waypoints start IN FRONT of
+        # the car...., we recheck below
+        # If the clost x,y behind the car, we just ignore it, and
+        # cut if off, and take next one point to replace it.
+
+        #check if closeset is ahead or behind vehicle
         closest_coord = self.waypoints_2d[closest_idx]
         prev_coord = self.waypoints_2d[closest_idx-1]
 
@@ -75,7 +84,9 @@ class WaypointUpdater(object):
 
         val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
 
-        if val > 0:#car current pose is ahead the closest pot
+        # the closest point now behind the car, we will force
+        # use the next point as closest point
+        if val > 0:
             closest_idx = (closest_idx +1) % len(self.waypoints_2d)
 
         return closest_idx
@@ -89,18 +100,22 @@ class WaypointUpdater(object):
 
     def pose_cb(self, msg):
         # TODO: Implement
-        # be called frequencetly, save the current position
+        # be called frequencetly, save the current car position
         self.pose = msg
         pass
 
     def waypoints_cb(self, waypoints_in):
         # TODO: Implement
-        # get base waypoints every frame/time
-        # although it
+        # get base waypoints with a singel call back
+
+        #just save the waypoints, waypoints will not change
         self.base_waypoints = waypoints_in
         #if we don't have waypoints_2d, create one
+        # we hope the sefl.waypoints_2d has initialzed before
+        # the susbscriber, which means this cb fun
+        # will be called before the self.waypoints_2d = None
+        # then the KD tree will get some unpredictable points.
         if not self.waypoints_2d:
-            #save the whole base_waypoint to KD_tree
             '''
             check the command
 
@@ -129,8 +144,10 @@ class WaypointUpdater(object):
                 float64 w
 
             '''
+
             self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints_in.waypoints]
-            #find the closetest point to the car with KD_tree later. log(n)
+            #later on , we can use the waypoint_tree to
+            #find the closetest point to the car with KD_tree. from n - > log(n)
             self.waypoint_tree = KDTree(self.waypoints_2d)
 
 
